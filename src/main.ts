ad03b30e0fa63ee3b831e4de2f86d6f1d79e30be
Sigit Prayitno <cybergitt@gmail.com>
2023-06-16 22:10:35 +0700

    Add nestjs/config

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { abortOnError: false });
+  await app.listen(
+    process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 3000,
+  );
+  console.log(
+    `App Running on ${process.env.APP_ENV} with Port ${process.env.APP_PORT}`,
+  );
 }
-bootstrap();
+bootstrap().catch((e) => {
+  console.error(e);
+});
