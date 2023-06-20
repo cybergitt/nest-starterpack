4feb1bd28c2cefaf3decc4578acb5dd8764933a8
Sigit Prayitno <cybergitt@gmail.com>
2023-06-20 11:38:24 +0700

    Add statuses module

---
+++ src/modules/statuses/entities/status.entity.ts
@@ -0,0 +1,16 @@
+import { EntityHelper } from '@common/utils/entity-helper';
+import { ApiProperty } from '@nestjs/swagger';
+import { Allow } from 'class-validator';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class Status extends EntityHelper {
+  @ApiProperty({ example: 1 })
+  @PrimaryColumn()
+  id: number;
+
+  @Allow()
+  @ApiProperty({ example: 'Active' })
+  @Column()
+  name?: string;
+}
